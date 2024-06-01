64789e5001086d431f5921abb2d3d60114982f47
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-06-01 21:48:55 +0800

    登入註冊切換panel

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import "bootstrap";
+import "./assets/all.scss";
+createApp(App).use(createPinia()).mount("#app");
