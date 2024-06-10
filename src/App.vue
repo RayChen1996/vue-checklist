5a57517c9e0624867ea65e38be7306cde3db751a
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-06-10 17:32:19 +0800

    首頁格線修正

--- src/App.vue
+++ src/App.vue
@@ -8,112 +8,92 @@ const habdleSwitch = (val: boolean) => {
 </script>
 
 <template>
-  <div
-    class="d-flex min-vh-100 bg-primary justify-content-center align-items-center"
-  >
-    <div class="row w-75">
-      <div class="col-12 col-md-6">
-        <div class="mt-5 mb-2">
-          <img src="/check.svg" alt="Check" />
-          <img src="/logo_text.svg" alt="Logo Text" />
+  <div class="min-vh-100 bg-primary d-flex align-items-center">
+    <div class="container">
+      <div class="row w-100">
+        <div class="col-12 col-md-6 d-flex flex-column align-items-center">
+          <div class="mt-5 mb-2 d-flex align-items-center">
+            <img src="/check.svg" alt="Check" class="me-2" />
+            <img src="/logo_text.svg" alt="Logo Text" />
+          </div>
+          <img
+            src="/login_left.png"
+            class="d-none d-md-block img-fluid"
+            alt=""
+          />
         </div>
-        <img src="/login_left.png" class="d-none d-md-block" alt="" />
-      </div>
-      <div class="col-12 col-md-6 w-50">
-        <div class="mt-5">
-          <h2 v-if="isReg" class="fw-bolder mb-6">註冊帳號</h2>
-
-          <h2 v-else class="fw-bolder mb-6">最實用的線上代辦事項服務</h2>
-        </div>
-
-        <div v-if="isReg">
-          <label class="fw-bolder mt-3" for="email">Email</label>
-          <input
-            class="form-control"
-            type="email"
-            id="email"
-            placeholder="請輸入Email"
-          />
-
-          <br />
-
-          <label class="fw-bolder" for="password">您的暱稱</label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="請輸入您的暱稱"
-          />
-
-          <br />
-
-          <label class="fw-bolder" for="password">密碼</label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="請輸入密碼"
-          />
-
-          <br />
-          <label class="fw-bolder" for="password">再次輸入密碼</label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="請再次輸入密碼"
-          />
-
-          <br />
-        </div>
-        <div v-else>
-          <label class="fw-bolder mt-3" for="email">Email</label>
-          <input
-            class="form-control"
-            type="email"
-            id="email"
-            placeholder="請輸入Email"
-          />
-
-          <br />
-
-          <label class="fw-bolder" for="password">密碼</label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="請輸入密碼"
-          />
-
-          <br />
-        </div>
-
         <div
-          v-if="isReg"
-          class="mx-auto bg-primary d-flex flex-column justify-content-center align-items-center"
+          class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center"
         >
-          <button class="btn btn-primary btn-lg fw-bolder w-25">
-            註冊帳號
-          </button>
-
-          <br />
-
-          <label class="fw-bolder" @click="habdleSwitch(false)">
-            <a>登入</a>
-          </label>
-        </div>
-
-        <div
-          v-else
-          class="mx-auto bg-primary d-flex flex-column justify-content-center align-items-center"
-        >
-          <button class="btn btn-primary btn-lg fw-bolder w-25">登入</button>
-
-          <br />
-
-          <label class="fw-bolder" @click="habdleSwitch(true)">
-            <a>註冊帳號</a>
-          </label>
+          <div class="mt-5 w-75">
+            <h2 v-if="isReg" class="fw-bolder mb-4">註冊帳號</h2>
+            <h2 v-else class="fw-bolder mb-4">最實用的線上代辦事項服務</h2>
+            <div v-if="isReg">
+              <label class="fw-bolder mt-3" for="email">Email</label>
+              <input
+                class="form-control mb-3"
+                type="email"
+                id="email"
+                placeholder="請輸入Email"
+              />
+              <label class="fw-bolder" for="nickname">您的暱稱</label>
+              <input
+                class="form-control mb-3"
+                type="text"
+                id="nickname"
+                placeholder="請輸入您的暱稱"
+              />
+              <label class="fw-bolder" for="password">密碼</label>
+              <input
+                class="form-control mb-3"
+                type="password"
+                id="password"
+                placeholder="請輸入密碼"
+              />
+              <label class="fw-bolder" for="confirm-password"
+                >再次輸入密碼</label
+              >
+              <input
+                class="form-control mb-3"
+                type="password"
+                id="confirm-password"
+                placeholder="請再次輸入密碼"
+              />
+            </div>
+            <div v-else>
+              <label class="fw-bolder mt-3" for="email">Email</label>
+              <input
+                class="form-control mb-3"
+                type="email"
+                id="email"
+                placeholder="請輸入Email"
+              />
+              <label class="fw-bolder" for="password">密碼</label>
+              <input
+                class="form-control mb-3"
+                type="password"
+                id="password"
+                placeholder="請輸入密碼"
+              />
+            </div>
+            <div class="d-flex flex-column align-items-center">
+              <button
+                v-if="isReg"
+                class="btn btn-primary btn-lg fw-bolder w-100 mb-3"
+              >
+                註冊帳號
+              </button>
+              <button
+                v-else
+                class="btn btn-primary btn-lg fw-bolder w-100 mb-3"
+              >
+                登入
+              </button>
+              <label class="fw-bolder" @click="habdleSwitch(!isReg)">
+                <a>{{ isReg ? "登入" : "註冊帳號" }}</a>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
